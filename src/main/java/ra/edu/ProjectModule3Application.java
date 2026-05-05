3b052dc29629aa1627dc7d7c1a437ebec50ef818
vanh251 <148326521+vanh251@users.noreply.github.com>
2026-05-05 19:43:47 +0700

    first commit

---
+++ src/main/java/ra/edu/ProjectModule3Application.java
@@ -0,0 +1,13 @@
+package ra.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectModule3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectModule3Application.class, args);
+    }
+
+}
